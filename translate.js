345b23d9d868653a5c0479853665c693ffda9ce5
Pablo Ferder <pferder@users.noreply.github.com>
2022-08-03 15:44:11 -0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,24 +1,28 @@
-
+let counter = 0; 
 let selects = [];
 
 const translate = () => {
     // cambiar CHOOSE a Elegir opción
 
-    for (i=0;i<selects.length;i++) {
-        selects[i]['children'][0]['innerHTML'] = 'Elegir opción';
+for (i=0;i<selects.length;i++) {
+    
+    selects[i]['children'][0]['innerHTML'] = 'Elegir opción';
     }   
 
 }
 
 const getSelects = () => {
+    counter++;
     selects = document.getElementsByClassName('mat-select-value');
     
     if(selects.length > 1) {
-        translate()
+            translate()
     } else {
-        console.log(selects)
+        clearInterval(interval)
     }
-     
+    
+    console.log(counter)
+    console.log(selects)
 }
 
-getSelects()
+let interval = setInterval(getSelects(),3000)
