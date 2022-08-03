4601796f396c0277f48ac2cca8b6da2bd5934f6a
Pablo Ferder <pferder@users.noreply.github.com>
2022-08-03 15:30:18 -0300

    Create translate.js

---
+++ translate.js
@@ -0,0 +1,25 @@
+
+let selects = [];
+
+const translate = () => {
+    // cambiar CHOOSE a Elegir opción
+
+    for (i=0;i<selects.length;i++) {
+        selects[i]['children'][0]['innerHTML'] = 'Elegir opción';
+    }   
+
+}
+
+const getSelects = () => {
+    selects = document.getElementsByClassName('mat-select-value');
+    
+    if(selects.length > 1) {
+        translate()
+    } else {
+        console.log(counter)
+        console.log(selects)
+    }
+     
+}
+
+getSelects()
